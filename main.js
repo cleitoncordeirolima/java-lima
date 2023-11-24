8bcbae7a8c1b0601f58f402cb2deafa0d1b6877b
cleitoncordeirolima <103213932+cleitoncordeirolima@users.noreply.github.com>
2023-11-24 08:48:39 -0300

    atualizando meu proje.

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
- function tocaSomPom() {
-   document.querySelector('#som_tecla_pom').play();
- }
+// Pom ----------------------------------------
+function tocaSomPom () {
+  document.querySelector('#som_tecla_pom').play();
+}
 
-document.querySelectorAll ('.tecla')
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas[0].onclick = tocaSomPom;
+
+let contador;
+
+//enquanto
+while(contador < listaDeTeclas.length){
+
+  listaDeTeclas[contador].onclick = tocaSomPom
+
+  contador = contador +1;
+
+  console.log(contador);
+}
