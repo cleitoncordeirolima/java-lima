83266c954e1212610c47c352987d586a5e79e4fd
cleitoncordeirolima <103213932+cleitoncordeirolima@users.noreply.github.com>
2024-03-20 09:21:54 -0300

    atualizando minha pág.

--- main.js
+++ main.js
@@ -1,18 +1,55 @@
-function tocaSom (idElementoAudio) {
-  document.querySelector(idElementoAudio).play();
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+        }
+
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+    }
 }
 
-const listaDeTeclas = document.querySelectorAll('.tecla');
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-10-08T00:00:00");
+const tempoObjetivo2 = new Date("2024-12-31T00:00:00");
+const tempoObjetivo3 = new Date("2024-12-31T00:00:00");
+const tempoObjetivo4 = new Date("2024-12-10T00:00:00");
 
-//para
-for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
-  const tecla = listaDeTeclas[contador];
-  const instrumento = tecla.classList[1];
-  const idAudio = `#som_${instrumento}`; //template string
 
-  tecla.onclick = function () {
-      tocaSom(idAudio);
-  }
+function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
 
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+    if (tempoFinal > 0){
+        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+    } else {
+        return "Prazo Finalizado";
+    }
 }
+
+function atualizaCronometro(){
+    for (let i=0; i<contadores.length;i++){
+        contadores[i].textContent = calculaTempo(tempos[i]);   
+    }
+}
+
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro,1000);
+}
+
+comecaCronometro();
